7a731723566f3642cb382455923df540a12b54af
biancarusso27-code <bianca.russo27@gmail.com>
2026-09-23 16:20:46 -0300

    atualizar fotos

--- script.js
+++ script.js
@@ -463,7 +463,7 @@ const pages = {
         <div class="photo-grid">
 
           <div class="photo">
-            <div class="placeholder-photo">LOOK 01</div>
+            <img src="images/looks/y2k/fem-01.jpg" alt="Look Y2K feminino 01" class="look-photo">
             LOOK 01
           </div>
 
