a16e875b35b4a29fa19fd001b33c5c22864b5351
biancarusso27-code <bianca.russo27@gmail.com>
2026-09-23 15:20:51 -0300

    atualizar style e script

--- script.js
+++ script.js
@@ -1027,7 +1027,7 @@ const pages = {
 </p>
 
     <div class="box invite">
-  <div class="box-title">DE REPENTE, 30! 💿</div>
+  <div class="box-title testimonial-title">~*~ DE REPENTE, 30! ~*~</div>
 
   <p>
     gnt, chegou a horaaaa 😭
