dea965dd20cfb599ab214f626913199508f5b936
biancarusso27-code <bianca.russo27@gmail.com>
2026-09-24 16:10:41 -0300

    Update script.js
    Adicionar looks masculinos Emo Rock

--- script.js
+++ script.js
@@ -606,25 +606,37 @@ const pages = {
 
         <div class="photo-grid">
 
-          <div class="photo">
-            <div class="placeholder-photo">LOOK 01</div>
-            LOOK 01
-          </div>
+          
+<div class="photo">
+  <img src="imagens/looks/looks%20emo-rock/emo-masculino-01.jpg"
+       alt="Inspiração Emo Rock masculino 01">
+  LOOK 01
+</div>
+    
 
-          <div class="photo">
-            <div class="placeholder-photo">LOOK 02</div>
-            LOOK 02
-          </div>
+          
+<div class="photo">
+  <img src="imagens/looks/looks%20emo-rock/emo-masculino-02.jpg"
+       alt="Inspiração Emo Rock masculino 02">
+  LOOK 02
+</div>
+    
 
-          <div class="photo">
-            <div class="placeholder-photo">LOOK 03</div>
-            LOOK 03
-          </div>
+         
+<div class="photo">
+  <img src="imagens/looks/looks%20emo-rock/emo-masculino-03.jpg"
+       alt="Inspiração Emo Rock masculino 03">
+  LOOK 03
+</div>
+    
 
-          <div class="photo">
-            <div class="placeholder-photo">LOOK 04</div>
-            LOOK 04
-          </div>
+         
+<div class="photo">
+  <img src="imagens/looks/looks%20emo-rock/emo-masculino-04.jpg"
+       alt="Inspiração Emo Rock masculino 04">
+  LOOK 04
+</div>
+    
 
         </div>
       </div>
